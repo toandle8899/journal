e22119b06f1ce9a50416005f2a6ac8e8f477214c
Duc Toan Le <led@debe1ml.global.umusic.net>
2026-02-04 18:52:38 +0100

    Fix: Bind to IPv4 and correct port for Fly.io deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,8 +20,8 @@ if System.get_env("PHX_SERVER") do
   config :journal, JournalWeb.Endpoint, server: true
 end
 
-config :journal, JournalWeb.Endpoint,
-  http: [port: String.to_integer(System.get_env("PORT", "4000"))]
+
+
 
 if config_env() == :prod do
   database_path =
@@ -54,11 +54,9 @@ if config_env() == :prod do
   config :journal, JournalWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      # Bind to all IPv4 interfaces
+      ip: {0, 0, 0, 0},
+      port: String.to_integer(System.get_env("PORT") || "8080")
     ],
     secret_key_base: secret_key_base
 
